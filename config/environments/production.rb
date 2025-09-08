0b312e274d5a63c5756feee0ef6dc18105bd7511
hel rabelo <helrabelo@gmail.com>
2025-09-08 13:06:56 -0300

    Fix production build asset precompilation issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: 'apikey',
-    password: ENV.fetch('SENDGRID_API_KEY'),
+    password: ENV.fetch('SENDGRID_API_KEY', nil),
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
   }
+  
+  # Allow assets to be precompiled without a real secret key base during build
+  config.require_master_key = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
